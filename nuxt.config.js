66aa41499fbdd65723c984b5d0bf553d05428278
Felipe Saa <fsechavarria@gmail.com>
2018-05-31 19:43:41 -0400

    Revert auth middleware mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ module.exports = {
   ** 'spa': No server-side rendering (only client-side navigation)
   ** 'universal': Isomorphic application (server-side rendering + client-side navigation)
   */
-  mode: 'spa',
+  mode: 'universal',
   /*
   ** Router config
   */
