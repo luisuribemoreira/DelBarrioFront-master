7ee96bdbfc3f849114c4199293b5c6cd34644d96
DeafeningRoar <finalx7@gmail.com>
2018-06-09 17:07:35 -0400

    Se modifica validacion de edad para que sea mas precisa

--- controllers/customvalidations.js
+++ controllers/customvalidations.js
@@ -28,13 +28,27 @@ function isDateAfter (before, after) {
 
 function isUnderAge (date) {
   if (isDate(date)) {
-    if (Number(new Date().getFullYear()) - Number(new Date(date).getFullYear()) < 18) {
-      return true
+    // Fecha del dia de hoy
+    let fechaActual = new Date()
+    // Fecha ingresada en el formulario
+    let fecha = new Date(date)
+    // Comprueba, en orden, si han pasado al menos 18 años de la fecha ingresada
+    // si el mes es mayor al actual
+    // y finalmente si el mes actual es el mismo al ingresado y el dia actual es mayor o igual al ingresado.
+    // Ej; Fecha actual: 10-05-2018, Fecha Ingresada: 11-05-2000 -> returns true, es menor de edad por 1 dia.
+    if (fechaActual.getFullYear() - fecha.getFullYear() <= 18) {
+      if (fechaActual.getMonth() > fecha.getMonth()) {
+        return false
+      } else if (fechaActual.getMonth() === fecha.getMonth() && fechaActual.getDate() >= fecha.getDate()) {
+        return false
+      } else {
+        return true
+      }
     } else {
-      return false
+      return true
     }
   } else {
-    return false
+    return true
   }
 }
 
