32ab79fc098378fae27901e24056e0fec4480494
DeafeningRoar <finalx7@gmail.com>
2018-06-09 17:48:16 -0400

    Cambio de mensajes de validacion a español

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
+import es from 'vee-validate/dist/locale/es'
 
-Vue.use(VeeValidate)
+Vue.use(VeeValidate, {
+  locale: 'es',
+  dictionary: {
+    es
+  }
+})
