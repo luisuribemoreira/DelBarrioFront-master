75ca833868b82403a69908a4dbaa17e62539f99f
Cesar Rojas Tapia <39638699+CesRojas@users.noreply.github.com>
2018-06-19 14:26:56 -0400

    Agregadas validaciones
    Agregada la validación de precio oferta, arregladas las validaciones de fecha

--- controllers/customvalidations.js
+++ controllers/customvalidations.js
@@ -14,16 +14,43 @@ function isDefined (value) {
   }
 }
 
-function isDateAfter (before, after) {
-  if (isDate(before) && isDate(after)) {
-    if (new Date(before) < new Date(after)) {
-      return true
-    } else {
-      return false
-    }
+function isOffer (before, after) {
+  if (before > after) {
+    return true
   } else {
     return false
   }
+}
+
+function inicio (fecha) {
+  var f = new Date()
+  var dd = f.getDate()
+  var mm = f.getMonth()
+  var yyyy = f.getFullYear()
+
+  var diaInput = fecha.getDate()
+  var mesInput = fecha.getMonth()
+  var anioInput = fecha.getFullYear()
+
+  if (diaInput + mesInput + anioInput >= dd + mm + yyyy) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function isDateAfter (before, after) {
+  var ddb = before.getDate()
+  var mmb = before.getMonth()
+  var yyyyb = before.getFullYear()
+
+  var dda = after.getDate()
+  var mma = after.getMonth()
+  var yyyya = after.getFullYear()
+
+  if (ddb + mmb + yyyyb < dda + mma + yyyya) {
+    return true
+  } else { return false }
 }
 
 function isUnderAge (date) {
@@ -47,6 +74,8 @@ function isUnderAge (date) {
 export default {
   isDate,
   isDefined,
+  isOffer,
+  inicio,
   isDateAfter,
   isUnderAge
 }
