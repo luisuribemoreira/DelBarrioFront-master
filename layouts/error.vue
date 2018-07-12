50c543353e3b9f3be7d27fdb5cfb9ce0c9afc07d
DeafeningRoar <finalx7@gmail.com>
2018-07-11 22:28:31 -0400

    Creacion de pagina de error para redireccion

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script>
+
+export default {
+  beforeMount () {
+    // Redirecciona cualquier error a la página de inicio
+    this.$notify.danger('Ha ocurrido un error inesperado')
+    this.$router.push({ path: '/' })
+  }
+}
+</script>
