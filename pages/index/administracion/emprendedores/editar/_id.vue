cec7090cf7867076b3be80135865ca0837f44156
SebastianFeliu <sebastianfeliu97@gmail.com>
2018-07-22 17:49:43 -0400

    (Admin)Se cambio la vista de editar emprendores junto con su controlador

--- pages/index/administracion/emprendedores/editar/_id.vue
+++ pages/index/administracion/emprendedores/editar/_id.vue
@@ -4,12 +4,9 @@
       <div class="row">
         <div class="col-md-6 col-md-offset-3 fondo-beige">
           <h2 class="text-center">Editar Emprendedor</h2>
+          <hr>
           <form @submit.prevent="validateBeforeSubmit">
-            <div class="form-group margin-top">
-              <label for="name">Nombre de fantasía</label>
-              <input v-validate data-vv-rules="required" data-vv-as="nombre" name="name" type="text" v-model="entrepreneur.DESC_NOMBRE_FANTASIA" class="form-control"/>
-              <small class="text-danger" v-show="errors.has('name')">{{ errors.first('name') }}</small>
-            </div>
+            <h4>Datos del Emprendimiento</h4>
             <div class="form-group margin-top">
               <label for="realname">Nombre Empresa</label>
               <input v-validate data-vv-rules="required" data-vv-as="nombre empresa" name="realname" type="text" v-model="entrepreneur.DESC_NOMBRE_EMPRESA" class="form-control"/>
@@ -21,11 +18,6 @@
               <small class="text-danger" v-show="errors.has('rut')">{{ errors.first('rut') }}</small>
             </div>
             <div class="form-group margin-top">
-              <label for="description">Descripcion</label>
-              <textarea v-validate data-vv-rules="required" data-vv-as="descripción" name="description" type="text" v-model="entrepreneur.DESC_EMPRENDEDOR" class="form-control"></textarea>
-              <small class="text-danger" v-show="errors.has('description')">{{ errors.first('description') }}</small>
-            </div>
-            <div class="form-group margin-top">
               <label for="workfield">Rubro</label>
               <select v-model="entrepreneur.IDEN_RUBRO" class="form-control" v-validate data-vv-rules="required" :size="5" name="workfield">
                 <option v-for="workfield in workfields" v-if="workfield.FLAG_VIGENTE" :key="workfield.IDEN_RUBRO" :value="workfield.IDEN_RUBRO">
@@ -33,6 +25,18 @@
                 </option>
               </select>
               <small class="text-danger" v-show="errors.has('workfield')">{{ errors.first('workfield') }}</small>
+            </div>
+            <hr>
+            <h4>Datos de emprendedor</h4>
+            <div class="form-group margin-top">
+              <label for="email">Correo electrónico</label>
+              <input v-validate data-vv-rules="required|email" data-vv-as="correo electrónico" name="email" type="text" v-model="entrepreneur.usuario.EMAIL_USUARIO" class="form-control"/>
+              <small class="text-danger" v-show="errors.has('email')">{{ errors.first('email') }}</small>
+            </div>
+            <div class="form-group margin-top">
+              <label for="pass">Contraseña</label>
+              <input v-validate data-vv-rules="required" data-vv-as="clave de municipalidad" name="pass" type="text" v-model="entrepreneur.usuario.DESC_PASSWORD" class="form-control"/>
+              <small class="text-danger" v-show="errors.has('pass')">{{ errors.first('pass') }}</small>
             </div>
             <button type="submit" class="btn btn-default">Guardar</button>
           </form>
