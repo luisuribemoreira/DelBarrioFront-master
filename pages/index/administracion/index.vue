67b356ebc39f5fd484c4c9b74aed3d399615d984
DeafeningRoar <finalx7@gmail.com>
2018-09-02 15:43:42 -0300

    Se agrega paginacion, 5 items por pagina.

--- pages/index/administracion/index.vue
+++ pages/index/administracion/index.vue
@@ -4,26 +4,27 @@
     <p><i>* Las estadísticas son compiladas con los datos del último mes</i></p>
     <br/>
     <div class="col-md-12 col-md-offset-3">
-        <button type="submit" class="btn btn-default" @click="denunciasPublicaciones()">Denuncias de Publicaciones</button>
+        <button type="submit" class="btn btn-default" @click="denunciasPublicaciones(), type = 'Denuncias de Publicaciones'">Denuncias de Publicaciones</button>
     </div>
     <br/>
     <div class="col-md-12 col-md-offset-3">
-        <button type="submit" class="btn btn-default" @click="publicacionesAprobadas()">Publicaciones Aprobadas</button>
+        <button type="submit" class="btn btn-default" @click="publicacionesAprobadas(), type = 'Publicaciones Aprobadas'">Publicaciones Aprobadas</button>
     </div>
     <br/>
     <div class="col-md-12 col-md-offset-3">
-        <button type="submit" class="btn btn-default" @click="publicacionesRechazadas()">Publicaciones Rechazadas</button>
+        <button type="submit" class="btn btn-default" @click="publicacionesRechazadas(), type = 'Publicaciones Rechazadas'">Publicaciones Rechazadas</button>
     </div>
     <br/><br/><br/>
     <div class="row margin-top" v-if="reportData.length > 0">
       <div class="col-12 table-responsive">
+        <h4 class="text-center">{{type}}</h4> <br/>
         <table class="table table-hover table-sm">
           <thead class="text-center">
             <tr>
               <th :key="header" v-for="header in headers">{{ header }}</th>
             </tr>
           </thead>
-          <tbody class="text-center" :key="data.publicacion" v-for="data in reportData">
+          <tbody class="text-center" :key="data.publicacion" v-for="data in paginatedData[pagination]">
             <tr data-toggle="collapse" @click="selected = data[1], open = !open">
               <nuxt-link v-if="data[1]" :to="{ path: '/publicaciones/' + data[1]}">
                 <td>Ir a la publicación</td>
@@ -42,31 +43,31 @@
             </tr>
           </tbody>
         </table>
-      <!-- <nav aria-label="Page navigation">
-        <ul class="pagination justify-content-center">
-          <li class="page-item">
-            <span aria-label="Previous" v-on:click="pagination > 0 ? pagination-- : ''">
-              <span class="page-link" :aria-hidden="true">&laquo;</span>
-            </span>
-          </li>
-          <li class="page-item" v-bind:key="page" v-for="page in pages">
-            <span class="page-link" v-bind:class="{ 'font-weight: bold' : pagination === page - 1 }" v-on:click="pagination = page - 1">{{ page }}</span>
-          </li>
-          <li class="page-item">
-            <span aria-label="Next" v-on:click="pagination < paginatedData.length - 1 ? pagination++ : ''">
-              <span class="page-link" :aria-hidden="true">&raquo;</span>
-            </span>
-          </li>
-        </ul>
-      </nav> --> <!-- navegacion --> 
+        <nav aria-label="Page navigation">
+          <ul class="pagination justify-content-center">
+            <li class="page-item">
+              <span aria-label="Previous" v-on:click="pagination > 0 ? pagination-- : ''">
+                <span class="page-link" :aria-hidden="true">&laquo;</span>
+              </span>
+            </li>
+            <li class="page-item" v-bind:key="page" v-for="page in pages">
+              <span class="page-link" v-bind:class="{ 'font-weight: bold' : pagination === page - 1 }" v-on:click="pagination = page - 1">{{ page }}</span>
+            </li>
+            <li class="page-item">
+              <span aria-label="Next" v-on:click="pagination < paginatedData.length - 1 ? pagination++ : ''">
+                <span class="page-link" :aria-hidden="true">&raquo;</span>
+              </span>
+            </li>
+          </ul>
+        </nav> <!-- navegacion --> 
       </div>
-        </div>
-
+    </div> <!-- Datos de reporte -->
   </div>
 </template>
 
 <script>
 import controllerReporteria from '~/controllers/admin/reporteria'
+import customPaginator from '~/controllers/custompaginator'
 
 export default {
   methods: {
@@ -75,6 +76,9 @@ export default {
       if (data !== -1) {
         this.reportData = data.reportData
         this.headers = data.headers
+        this.paginatedData = (await customPaginator.paginate(this.reportData, 5)).paginatedData
+        this.pages = this.paginatedData.length
+        this.pagination = 0
       }
     },
     async publicacionesAprobadas () {
@@ -83,6 +87,9 @@ export default {
       if (data !== -1) {
         this.reportData = data.reportData
         this.headers = data.headers
+        this.paginatedData = (await customPaginator.paginate(this.reportData, 5)).paginatedData
+        this.pages = this.paginatedData.length
+        this.pagination = 0
       }
     },
     async publicacionesRechazadas () {
@@ -91,6 +98,9 @@ export default {
       if (data !== -1) {
         this.reportData = data.reportData
         this.headers = data.headers
+        this.paginatedData = (await customPaginator.paginate(this.reportData, 5)).paginatedData
+        this.pages = this.paginatedData.length
+        this.pagination = 0
       }
     }
   },
@@ -100,7 +110,11 @@ export default {
       selected: false,
       headers: [],
       reportData: [],
-      textWeight: 'font-weight-bold'
+      textWeight: 'font-weight-bold',
+      type: '',
+      pagination: 0,
+      pages: 0,
+      paginatedData: [[]]
     }
   },
   head () {
