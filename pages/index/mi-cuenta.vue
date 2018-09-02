e3a8925e3927968606e7e2b0aca93664853b2574
DeafeningRoar <finalx7@gmail.com>
2018-09-02 17:32:22 -0300

    Se implementa funciones para mostrar reportes

--- pages/index/mi-cuenta.vue
+++ pages/index/mi-cuenta.vue
@@ -22,6 +22,71 @@
       </div>
       <hr>
       <div v-if="isAuthenticated && loggedUser.rol === 102">
+        <h2>Listado De Reportes</h2>
+        <p><i>* Las estadísticas son compiladas con los datos del último mes</i></p>
+        <br/>
+        <div class="row">
+          <div class="col-md-4">
+              <button type="submit" class="btn btn-default" @click="ProductosPorVisitas(), type = 'Publicaciones por Visitas'">Publicaciones por Visitas</button>
+          </div>
+          <div class="col-md-4">
+              <button type="submit" class="btn btn-default" @click="ProductosPorValoracion(), type = 'Publicaciones por Valoración'">Publicaciones por Valoración</button>
+          </div>
+          <div class="col-md-4">
+              <button type="submit" class="btn btn-default" @click="ComentariosPorPublicacion(), type = 'Comentarios Por Publicación'">Comentarios Por Publicación</button>
+          </div>
+        </div>
+        <br/><br/>
+        <div class="row margin-top" v-if="reportData.length > 0"> <!-- Datos de reporte -->
+          <div class="col-12 table-responsive">
+            <h4 class="text-center">{{type}}</h4> <br/>
+            <table class="table table-hover table-sm">
+              <thead class="text-center">
+                <tr>
+                  <th :key="header" v-for="header in headers">{{ header }}</th>
+                </tr>
+              </thead>
+              <tbody class="text-center" :key="data.publicacion" v-for="data in paginatedData[pagination]">
+                <tr data-toggle="collapse" @click="data[6] && data[6].length > 0 ? selected = data[1] : '', data[6] && data[6].length > 0 ? open = !open : ''">
+                  <nuxt-link v-if="data[1]" :to="{ path: '/publicaciones/' + data[1]}">
+                    <td>Ir a la publicación</td>
+                  </nuxt-link>
+                  <td>{{data[1]}}</td>
+                  <td v-bind:class="[data[6] && data[6].length > 0 ? textWeight : '']"><icon v-if="data[6] && data[6].length > 0" name="angle-down"></icon>{{data[2]}}</td>
+                  <td>{{data[3]}}</td>
+                  <td>{{data[4]}}</td>
+                  <td>{{data[5]}}</td>
+                </tr>
+                <tr :id="data[1]" v-if="data[6] && data[6].length > 0 && data[1] == selected && open" v-for="comentario in data[6]" :key="comentario[2]" style="font-size: 90%">
+                    <td>{{comentario[1]}}</td> <!-- Linea para alinear datos con los headers -->
+                    <td>{{comentario[1]}}</td>
+                    <td>{{comentario[2]}}</td>
+                    <td>{{comentario[3]}}</td>
+                    <td>{{comentario[4]}}</td>
+                    <td>{{comentario[5]}}</td>
+                </tr>
+              </tbody>
+            </table>
+            <nav aria-label="Page navigation">
+              <ul class="pagination justify-content-center">
+                <li class="page-item">
+                  <span aria-label="Previous" v-on:click="pagination > 0 ? pagination-- : ''">
+                    <span class="page-link" :aria-hidden="true">&laquo;</span>
+                  </span>
+                </li>
+                <li class="page-item" v-bind:key="page" v-for="page in pages">
+                  <span class="page-link" v-bind:class="{ 'font-weight: bold' : pagination === page - 1 }" v-on:click="pagination = page - 1">{{ page }}</span>
+                </li>
+                <li class="page-item">
+                  <span aria-label="Next" v-on:click="pagination < paginatedData.length - 1 ? pagination++ : ''">
+                    <span class="page-link" :aria-hidden="true">&raquo;</span>
+                  </span>
+                </li>
+              </ul>
+            </nav> <!-- navegacion --> 
+          </div>
+        </div> <!-- Datos de reporte -->
+        <small class="text-danger" v-if="message">{{ message }}</small>
         <div class="row margin-top">
           <div class="col-12">
             <h3>Tus destacados</h3>
@@ -165,6 +230,8 @@ import controllerPosts from '~/controllers/posts'
 import customValidations from '~/controllers/customvalidations'
 import commentsController from '~/controllers/comments'
 import Datepicker from 'vuejs-datepicker'
+import controllerReporteria from '~/controllers/admin/reporteria'
+import customPaginator from '~/controllers/custompaginator'
 
 export default {
   asyncData ({ app, store }) {
@@ -198,7 +265,6 @@ export default {
               if (!user.persona.contacto.Direccion) user.persona.contacto.Direccion = [{}]
               if (!user.persona.contacto.Celular) user.persona.contacto.Celular = [{}]
               if (!user.persona.contacto.Correo) user.persona.contacto.Correo = [{}]
-              console.log(user.emprendedor)
               return {
                 user: user,
                 posts: posts,
@@ -229,7 +295,17 @@ export default {
       imageUrl: process.env.imagesUrl,
       entrepreneur: {},
       preguntas: 0,
-      user: {}
+      user: {},
+      open: false,
+      selectedItem: false,
+      headers: [],
+      reportData: [],
+      textWeight: 'font-weight-bold',
+      type: '',
+      pagination: 0,
+      pages: 0,
+      paginatedData: [[]],
+      message: false
     }
   },
   components: {
@@ -277,6 +353,57 @@ export default {
           }
         }
       })
+    },
+    async ProductosPorVisitas () {
+      let data = await controllerReporteria.productosPorVisitas(this)
+
+      if (data !== -1) {
+        this.reportData = data.reportData
+        this.headers = data.headers
+        this.paginatedData = (await customPaginator.paginate(this.reportData, 5)).paginatedData
+        this.pages = this.paginatedData.length
+        this.pagination = 0
+      }
+
+      if (this.reportData.length === 0) {
+        this.message = 'No se encontro ninguna publicación.'
+      } else {
+        this.message = false
+      }
+    },
+    async ProductosPorValoracion () {
+      let data = await controllerReporteria.productosPorValoracion(this)
+
+      if (data !== -1) {
+        this.reportData = data.reportData
+        this.headers = data.headers
+        this.paginatedData = (await customPaginator.paginate(this.reportData, 5)).paginatedData
+        this.pages = this.paginatedData.length
+        this.pagination = 0
+      }
+
+      if (this.reportData.length === 0) {
+        this.message = 'No se encontro ninguna publicación.'
+      } else {
+        this.message = false
+      }
+    },
+    async ComentariosPorPublicacion () {
+      let data = await controllerReporteria.comentariosPorProducto(this)
+
+      if (data !== -1) {
+        this.reportData = data.reportData
+        this.headers = data.headers
+        this.paginatedData = (await customPaginator.paginate(this.reportData, 5)).paginatedData
+        this.pages = this.paginatedData.length
+        this.pagination = 0
+      }
+
+      if (this.reportData.length === 0) {
+        this.message = 'No se encontro ninguna publicación.'
+      } else {
+        this.message = false
+      }
     }
   },
   computed: mapGetters([
