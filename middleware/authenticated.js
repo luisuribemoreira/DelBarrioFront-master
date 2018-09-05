c9b5e3d387e2ceb4b5ece003b8bd3249b125f3d4
DeafeningRoar <finalx7@gmail.com>
2018-09-05 20:44:20 -0300

    Se agregan funciones para redireccionar en base a los roles y el estado de autenticacion

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,5 +1,87 @@
-export default function ({ store, redirect }) {
+/**
+ * Este middleware se agrega directamente a las páginas
+ * No a nuxt.config.js
+ */
+export default function ({ store, redirect, route }) {
+  // Si el usuario no está autenticado, redirecciona a /autenticar.
   if (!store.getters.isAuthenticated) {
-    return redirect('/sign-in')
+    return redirect('/autenticar')
+  }
+  if (store.getters.loggedUser.rol !== 102) {
+    routesEntrepreneur(route.path, redirect)
+  }
+  if (store.getters.loggedUser.rol !== 103 && store.getters.loggedUser.rol !== 104) {
+    routesAdmin(route.path, redirect)
+  }
+  if (store.getters.loggedUser.rol !== 104) {
+    routesSuperAdmin(route.path, redirect)
+  }
+}
+
+// Rutas a las que puede acceder solo el emprendedor, rol 102.
+function routesEntrepreneur (path, redirect) {
+  // Restringir todas las rutas de las publicaciones en /administracion/publicaciones/...
+  // Nueva, Editar, Crear Oferta, Mis Publicaciones
+  if (path.match(/\/administracion\/publicaciones/ig)) {
+    return redirect('/')
+  }
+}
+
+// Rutas a las que puede acceder solo el administrador y super administrador, roles 103 y 104.
+function routesAdmin (path, redirect) {
+  // Restringir todas las rutas de moderacion en /administracion/moderar...
+  // Moderar Publicacion y Moderar Oferta
+  if (path.match(/\/administracion\/moderar/ig)) {
+    return redirect('/')
+  }
+  // Restringir ruta para administracion de denuncias en /administracion/denuncias
+  if (path.match(/\/administracion\/denuncias/ig)) {
+    return redirect('/')
+  }
+  // Restringir ruta para administracion de emprendedores en /administracion/emprendedores
+  // Nuevo y Editar
+  if (path.match(/\/administracion\/emprendedores/ig)) {
+    return redirect('/')
+  }
+  // Restringir ruta para administracion de clientes en /administracion/clientes
+  if (path.match(/\/administracion\/clientes/ig)) {
+    return redirect('/')
+  }
+  // Restringir ruta para los Reportes en /administracion
+  if (path.match(/^\/administracion$/i) || path.match(/^\/administracion\/$/i)) {
+    return redirect('/')
+  }
+  // Restringir ruta para administrar las categorias en /administracion/categorias
+  // Index (Listado especial para administrarlas), Nueva y Editar
+  if (path.match(/\/administracion\/categorias/gi)) {
+    return redirect('/')
+  }
+  // Restringir ruta para administrar los rubros en /administracion/rubros
+  // Index (Listado especial para administrarlos), Nueva y Editar
+  if (path.match(/\/administracion\/rubros/gi)) {
+    return redirect('/')
+  }
+  // Restringir ruta para administrar las razones de denuncia en /administracion/razon...
+  // Razon Denuncia y Razon de Desactivacion
+  if (path.match(/\/administracion\/razon/gi)) {
+    return redirect('/')
+  }
+  // Restringir ruta para administrar los terminos y condiciones en /administracion/terminos-condiciones
+  if (path.match(/\/administracion\/terminos-condiciones/gi)) {
+    return redirect('/')
+  }
+  // Restringir ruta para administrar las preguntas frecuentes en /administracion/preguntas-frecuentes
+  // Index (Listado especial para administrarlas), Nueva y Editar.
+  if (path.match(/\/administracion\/preguntas-frecuentes/gi)) {
+    return redirect('/')
+  }
+}
+
+// Rutas a las que puede acceder solo el super administrador, role 104.
+function routesSuperAdmin (path, redirect) {
+  // Restringir ruta para administrar a los administradores en /administracion/administradores
+  // Index (Listado especial para administrarlos), Nuevo y Editar.
+  if (path.match(/\/administracion\/administradores/gi)) {
+    return redirect('/')
   }
 }
