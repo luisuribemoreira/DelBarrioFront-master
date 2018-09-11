86315436ff57b87c817c8c43ac6c0cd98b2a6897
SebastianFeliu <sebastianfeliu97@gmail.com>
2018-09-11 13:18:02 -0300

    (Emprendedor) Se filtran las publicaciones que son baneadas y aun no validadas

--- pages/index/emprendedores/_id.vue
+++ pages/index/emprendedores/_id.vue
@@ -57,7 +57,7 @@
             </div>
  <div class="row">
           <transition-group name="ListPosts" tag="div">
-            <div class="col-md-2 col-sm-3 col-xs-6 post-item" v-for="p in entrepreneur.publicaciones" :key="p.IDEN_PUBLICACION">
+            <div class="col-md-2 col-sm-3 col-xs-6 post-item" v-for="p in entrepreneur.publicaciones" :key="p.IDEN_PUBLICACION" v-if="p.FLAG_VIGENTE && !p.FLAG_BAN && p.FLAG_VALIDADO">
               <nuxt-link :to="'/publicaciones/'+p.IDEN_PUBLICACION ">
                 <img v-if="p.imagenes.length === 0" v-lazy="'/img/no-image.svg'" class="img-responsive" alt="">
                 <img v-else v-lazy="imageUrl + p.imagenes[0].URL_IMAGEN" class="img-responsive" alt="">
