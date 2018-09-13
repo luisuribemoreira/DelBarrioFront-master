d9fa13cdd8c9412a87299116732d0f72161e0f6e
Ignacio Isaac Antillanca Gutiérrez <34189483+zockor1@users.noreply.github.com>
2018-09-13 18:12:19 -0300

    Limpieza página de Inicio

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,14 +3,8 @@
     <div class="container">
       <div class="row">
         <div class="col-md-10 offset-md-1">
-          <form @submit.prevent="validateBeforeSubmit">
-            <div class="form-group py-2">
-              <input class="form-control input-lg" placeholder="¿Qué buscas?"/>
-            </div>
-          </form>
           <center>
-          <img class="margin-top" style="text-align: center" :src="'https://i.ytimg.com/vi/vUQUDS-TL6Q/hqdefault.jpg'">
-          <h2><nuxt-link to="/busqueda-avanzada">Manual de usuario</nuxt-link></h2>
+          <!-- <h2><nuxt-link to="/busqueda-avanzada">Manual de usuario</nuxt-link></h2> -->
           <h2><nuxt-link to="/busqueda-avanzada">Búsqueda avanzada</nuxt-link></h2>
           </center>
         </div>
