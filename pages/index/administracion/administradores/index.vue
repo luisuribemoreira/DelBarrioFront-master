009b887c7be8bec41018f7817aad7328bdebca84
Ignacio Isaac Antillanca Gutiérrez <34189483+zockor1@users.noreply.github.com>
2018-09-16 20:02:45 -0300

    Se agrega buscador y paginador a tabla de administradores

--- pages/index/administracion/administradores/index.vue
+++ pages/index/administracion/administradores/index.vue
@@ -2,27 +2,27 @@
   <section id="admin-faq" class="container-fluid">
     <div class="container fondo-beige">
       <div class="row">
-        <div class="col-xs-12">
+        <div class="col-12">
           <h2 class="text-center">Administradores</h2>
         </div>
       </div>
       <div class="row">
-        <div class="col-md-4 col-sm-6 margin-top">
+        <div class="col-lg-1 col-md-4 py-2">
           <nuxt-link to="/administracion/administradores/nuevo" class="btn btn-tabla"><icon name="plus"></icon> Agregar</nuxt-link>
         </div>
-        <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 margin-top">
-          <form class="input-group text-truncate">
-            <input class="form-control" name="search" placeholder="Buscar" autocomplete="off" autofocus="autofocus" type="text">
+        <div class="col-lg-4 offset-md-3 col-md-6 offset-sm-3 py-1">
+          <div class="input-group text-truncate">
+            <input class="form-control" name="search" v-model="search" placeholder="Buscar nombre de administrador..." autocomplete="off" autofocus="autofocus" type="text" @keyup="buscarAdmin()">
             <div class="input-group-btn">
-              <button class="btn btn-outline-success" type="submit"><icon name="search" :aria-hidden="true"></icon></button>
+              <icon name="search"></icon>
             </div>
-          </form>
+          </div>
         </div>
       </div>
-      <div class="row margin-top">
-        <div class="col-xs-12 table-responsive">
-          <table class="table table-hover table-condensed">
-            <thead>
+      <div class="row py-2">
+        <div class="col-12 table-responsive">
+          <table class="table table-hover table-sm">
+            <thead class="text-center">
               <tr>
                 <th>Estado</th>  
                 <th>E-mail</th>
@@ -30,41 +30,47 @@
                 <th>Acción</th>
               </tr>
             </thead>
-            <tbody>
-              <tr :key="client.IDEN_PERSONA" v-for="client in clients">
-                <td><icon :name="client.usuario.FLAG_BAN ? 'times' : 'check'" :title="client.usuario.FLAG_BAN ? 'Deshabilitado' : 'Habilitado'"></icon></td>
+            <tbody class="text-center">
+              <tr :key="client.IDEN_PERSONA" v-for="client in paginatedData[pagination]">
+                <td>
+                  <icon :name="client.usuario.FLAG_BAN ? 'times' : 'check'" :title="client.usuario.FLAG_BAN ? 'Deshabilitado' : 'Habilitado'"></icon>
+                  </td>
                 <td>{{client.usuario.EMAIL_USUARIO}}</td>
                 <td>{{client.NOMBRES + ' ' + client.APELLIDO_PATERNO+ ' ' +client.APELLIDO_MATERNO}}</td>
                 <td>
-                  <nuxt-link :to="{ path: '/administracion/administradores/editar/'+client.IDEN_USUARIO }" class="btn btn-secondary">
+                  <nuxt-link :to="{ path: '/administracion/administradores/editar/'+client.IDEN_USUARIO }">
                     <button class="btn btn-tabla" title="Editar">Editar</button>
                   </nuxt-link>
-                  <a @click="setState(client)">
-                    <button class="btn" v-if="client.usuario.FLAG_BAN">Habilitar</button>
-                    <button class="btn" v-if="!client.usuario.FLAG_BAN">Deshabilitar</button>
+                  <a @click="setState(client)" class="btn">
+                    <button class="btn btn-tabla" v-if="client.usuario.FLAG_BAN">Habilitar</button>
+                    <button class="btn btn-tabla" v-if="!client.usuario.FLAG_BAN">Deshabilitar</button>
                   </a>
                 </td>
               </tr>
             </tbody>
           </table><!-- /tabla -->
-                      
-          <nav aria-label="Page navigation">
-            <ul class="pagination">
-              <li>
-                <a href="#" aria-label="Previous">
-                  <span :aria-hidden="true">&laquo;</span>
-                </a>
+               <nav aria-label="Page navigation">
+            <ul class="pagination justify-content-center">
+              <li class="page-item">
+                <!-- Solo permite retroceder si la pagina actual es mayor a 0 -->
+                <span aria-label="Previous" v-on:click="pagination > 0 ? pagination-- : ''">
+                  <span class="page-link" :aria-hidden="true">&laquo;</span>
+                </span>
               </li>
-              <li><a href="#">1</a></li>
-              <li><a href="#">2</a></li>
-              <li><a href="#">3</a></li>
-              <li>
-                <a href="#" aria-label="Next">
-                  <span :aria-hidden="true">&raquo;</span>
-                </a>
+              <!-- Se crea la paginacion al pie de pagina. Se usa page - 1 ya que pagination debe apuntar a los indices del arreglo, por lo que parte de 0 -->
+              <li class="page-item" v-bind:key="page" v-for="page in pages">
+                <!-- Si la pagina actual es igual a la clickeada, esta se ennegrece -->
+                <span class="page-link" v-bind:class="{ 'font-weight: bold' : pagination === page - 1 }" v-on:click="pagination = page - 1">{{ page }}</span>
+              </li>
+              <li class="page-item">
+                <!-- Solo permite avanzar si la pagina actual es inferior a la cantidad de paginas totales - 1 -->
+                <span aria-label="Next" v-on:click="pagination < paginatedData.length - 1 ? pagination++ : ''">
+                  <span class="page-link" :aria-hidden="true">&raquo;</span>
+                </span>
               </li>
             </ul>
-          </nav>
+          </nav>         
+        
         <!-- /paginacion -->              
         </div>
       </div>
@@ -74,14 +80,83 @@
 
 <script>
 import controller from '~/controllers/admin/admins'
+import custompaginator from '~/controllers/custompaginator'
 
 export default {
   asyncData ({ app }) {
     return controller.GETAll(app)
+      .then(({ clients }) => {
+        return custompaginator.paginate(clients)
+          .then(({ paginatedData }) => {
+            let pages = paginatedData.length
+            return {
+              clients,
+              paginatedData,
+              pages
+            }
+          })
+      })
+  },
+  data () {
+    return {
+      clients: [],
+      search: '',
+      postsAux: [],
+      pagination: 0,
+      pages: 0,
+      paginatedData: [[]]
+    }
   },
   methods: {
     setState (client) {
       controller.setState(this, client)
+    },
+    buscarAdmin () {
+      // Copiar todos los clientes, si existen, a una variable auxiliar para no perder la lista original
+      if (this.postsAux.length === 0) {
+        this.postsAux = this.paginatedData
+      }
+
+      // Si hay algo escrito en el buscador...
+      if (this.search.length > 0) {
+        // Se buscan todos los clientes en que el nombre, apellidos o parte de ellos posea el texto escrito en el buscador
+        let cadena = this.search.trim().split(' ')
+        let cliSearch = this.clients.map(client => {
+          for (let i = 0; i < cadena.length; i++) {
+            let palabraBusqueda = new RegExp(cadena[i], 'gi')
+            if (client.NOMBRES.match(palabraBusqueda) || client.APELLIDO_PATERNO.match(palabraBusqueda) ||
+                client.APELLIDO_MATERNO.match(palabraBusqueda)) return client
+          }
+        })
+
+        // Limpia los clientes actuales y lo llena con los clientes que cumplan el criterio de busqueda
+        let cliFound = []
+        cliSearch.forEach(client => {
+          if (client) cliFound.push(client)
+        })
+
+        // Ordena los Clientes en orden lexicografico.
+        cliFound.sort(function (a, b) {
+          return a.NOMBRES.localeCompare(b.NOMBRES)
+        })
+        // Se llama al paginador con la lista de objetos encontrados
+        custompaginator.paginate(cliFound)
+          .then(({ paginatedData }) => {
+            // Se reemplaza la lista paginada actual por la que contiene solo los objetos de la busqueda
+            this.paginatedData = paginatedData
+            // La cantidad total de paginas se reemplaza por la cantidad de paginas
+            // de la nueva lista de objetos encontrados por la busqueda
+            this.pages = paginatedData.length
+            this.pagination = 0 // Se envia a la pagina inicial en caso de que la busqueda contenga mas de 10 objetos
+          })
+      }
+
+      // Si no hay texto en el buscador se restaura la lista original
+      if (this.search.length === 0) {
+        this.paginatedData = this.postsAux
+        this.pagination = 0
+        this.pages = this.paginatedData.length
+      }
     }
   },
   middleware: 'authenticated',
