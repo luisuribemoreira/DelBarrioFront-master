66ba93f3d3d2b5c0a4cf3731a6201f8086da6340
DeafeningRoar <finalx7@gmail.com>
2018-09-17 20:52:45 -0300

    Página 404 Custom con redireccion automática

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container mt-5 text-center">
+        <h1 v-if="error.statusCode === 404">Error 404</h1>
+        <h1 v-if="error.statusCode === 404">Página no encontrada.</h1>
+        <h1 v-else>Ha ocurrido un error.</h1>
+        <h5>Serás redireccionado automáticamente al inicio dentro de 10 segundos.</h5>
+        <h5><nuxt-link to="/">Has click aquí para volver enseguida.</nuxt-link></h5>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  mounted () {
+    setTimeout(() => {
+      this.$router.push({ path: '/' })
+    }, 10000)
+  },
+  head () {
+    return {
+      title: '404'
+    }
+  }
+}
+</script>
