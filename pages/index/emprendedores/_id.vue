c24ebb82316ff3d88c13efe90432c61e779afded
dlmdv <dlmdv@users.noreply.github.com>
2018-09-17 22:33:23 -0300

    Carrousel publicaciones emprendedores

--- pages/index/emprendedores/_id.vue
+++ pages/index/emprendedores/_id.vue
@@ -103,7 +103,100 @@
     </div>
   </section>
   <!-- EMPRENDEDOR -->
-  
+  <section class="mb-5">
+
+    <style>
+    .blog .carousel-indicators {
+      left: 0;
+      top: auto;
+      bottom: -40px;
+
+    }
+
+    /* The colour of the indicators */
+    .blog .carousel-indicators li {
+      background: #a3a3a3;
+      border-radius: 50%;
+      width: 8px;
+      height: 8px;
+    }
+
+    .blog .carousel-indicators .active {
+      background: #707070;
+    }
+    </style>
+    <div class="container">
+      <div class="row blog">
+        <div class="col-md-12">
+          <div id="blogCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#blogCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#blogCarousel" data-slide-to="1"></li>
+            </ol>
+            <!-- Carousel items -->
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="row">
+                  <div class="col-md-3">
+                    <a href="#">
+                      <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                    </a>
+                  </div>
+                  <div class="col-md-3">
+                    <a href="#">
+                      <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                    </a>
+                  </div>
+                  <div class="col-md-3">
+                    <a href="#">
+                      <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                    </a>
+                  </div>
+                  <div class="col-md-3">
+                    <a href="#">
+                      <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                    </a>
+                  </div>
+                </div>
+                <!--.row-->
+              </div>
+              <!--.item-->
+              <div class="carousel-item">
+                <div class="row">
+                  <div class="col-md-3">
+                    <a href="#">
+                      <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                    </a>
+                  </div>
+                  <div class="col-md-3">
+                    <a href="#">
+                      <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                    </a>
+                  </div>
+                  <div class="col-md-3">
+                    <a href="#">
+                      <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                    </a>
+                  </div>
+                  <div class="col-md-3">
+                    <a href="#">
+                      <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                    </a>
+                  </div>
+                </div>
+                <!--.row-->
+              </div>
+              <!--.item-->
+            </div>
+            <!--.carousel-inner-->
+          </div>
+          <!--.Carousel-->
+        </div>
+      </div>
+    </div>
+</section>
+
+
   <!-- MAPA -->
   <section class="entrepreneur-map">
     <div class="container-fluid">
