a4d8e74e35eeefec6477ede3872ef636b935580e
dlmdv <dlmdv@users.noreply.github.com>
2018-09-17 22:15:05 -0300

    Correcion de margen del mapa

--- pages/index/emprendedores/_id.vue
+++ pages/index/emprendedores/_id.vue
@@ -107,7 +107,7 @@
   <!-- MAPA -->
   <section class="entrepreneur-map">
     <div class="container-fluid">
-      <div class="row">
+      <div class="row" style="margin-right: 0px;margin-left: 0px;">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.5792240152987!2d-70.62953658541339!3d-33.434212880778546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c58680ccc59f%3A0x35d790eaf21c17f2!2sAv.+Providencia+544%2C+Providencia%2C+Regi%C3%B3n+Metropolitana!5e0!3m2!1ses!2scl!4v1537015968767" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
     </div>
