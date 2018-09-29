ae19fcdde1b03585f54a8ad6adb902b417fe09ec
Ignacio Isaac Antillanca Gutiérrez <34189483+zockor1@users.noreply.github.com>
2018-09-29 13:10:28 -0300

    Se enlaza diseño de Faq

--- pages/index/preguntas-frecuentes.vue
+++ pages/index/preguntas-frecuentes.vue
@@ -1,19 +1,20 @@
 <template>
-  <section id="faq" class="container-fluid">
+  <section id="faq" class="faq section">
     <div class="container">
       <div class="row">
-        <div class="col-md-10 col-md-offset-1 fondo-beige">
-          <h2 class="text-center">Preguntas Frecuentes</h2>
-          <br> <!--cambiar-->
-          <div class="panel-group" :key="f.IDEN_FAQ" v-for="f in faqs">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h4 class="panel-title">
-                  <a data-toggle="collapse" :href="'#collapse'+f.IDEN_FAQ">{{f.NOMB_FAQ}}<i class="pull-right fa fa-angle-down" :aria-hidden="true"></i></a>
-                </h4>
+        <div class="faq--content col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+          <h2 class="h2 text-center">Preguntas Frecuentes</h2>
+          <div class="accordion mt-4" :key="f.IDEN_FAQ" v-for="f in faqs">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="mb-0">
+                  <button class="btn btn-link" type="button">
+                    <i data-toggle="collapse" :href="'#collapse'+f.IDEN_FAQ" aria-controls="collapse">{{f.NOMB_FAQ}}</i>
+                  </button>
+                </h5>
               </div>
-              <div :id="'collapse'+ f.IDEN_FAQ" class="panel-collapse collapse">
-                <div class="panel-body">{{f.DESC_FAQ}}</div>
+              <div :id="'collapse'+ f.IDEN_FAQ" class="panel-collapse collapse in">
+                <div class="card-body">{{f.DESC_FAQ}}</div>
               </div>
             </div>
           </div>
