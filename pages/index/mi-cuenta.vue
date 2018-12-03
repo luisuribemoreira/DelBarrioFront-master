403bab12836c3bdaf2ee0227232a91e157988f97
Sebastián Feliú <sebastianfeliu97@gmail.com>
2018-12-03 11:44:35 -0300

    Centrado de formularios Mi-Cuenta

--- pages/index/mi-cuenta.vue
+++ pages/index/mi-cuenta.vue
@@ -124,6 +124,7 @@
         </div>
         <hr class="margin-top">
       </div>
+      <div class="pb-3">
       <!-- Boton de Configuracion de cuenta Emprendedor -->
       <a class="btn btn-secondary" v-if="loggedUser.rol === 102" @click="selected = !selected">
         <icon name="wrench" :aria-hidden="true"></icon>
@@ -139,13 +140,16 @@
         <icon name="wrench" :aria-hidden="true"></icon>
         <span> Configuración de contraseña</span>
       </a>
+      </div>
+
       <!-- CONFIGURACION DE CUENTA -->
       <div v-if="selected" class="margin-top">
         <div>
-          <h2 class="text-center"><span>Datos de emprendedor</span></h2>
+          <h2 class="text-center pt-4"><span>Datos de emprendedor</span></h2>
           <hr>
         </div>
-        <div class="col-lg-6">
+        <div class="row justify-content-center">
+        <div class="col-lg-6 pt-5 ">
           <form @submit.prevent="validateBeforeSubmit">
             <div class="form-group margin-top">
               <label for="name">Nombres</label><span style="color: grey"> (Formato ej: Manuel Antonio)</span>
@@ -182,8 +186,8 @@
                 <h2 class="text-center"><span>Datos de Empresa</span></h2>
                 <hr>
               </div>
-              <div class="row">
-                <div class="margin-left col-md-6">
+              <div class="row justify-content-center mr-auto">
+                <div class="margin-left col-md-5 border">
                   <no-ssr>
                   <croppa :width="200"
                           :height="200"
@@ -199,7 +203,7 @@
               <div v-if="dataErrorMsg.error_foto">
                 <small class="text-danger">{{ dataErrorMsg.error_foto }}</small>
               </div>
-              <div class="form-group margin-top">
+              <div class="form-group margin-top pt-3">
                 <label for="name">Nombre de Fantasía</label><span style="color: grey"> (Formato ej: Artesanías Doña Anita)</span>
                 <input v-validate data-vv-rules="required" data-vv-as="nombre fantasia" name="nombre fantasia" type="text" v-model.trim="user.emprendedor.DESC_NOMBRE_FANTASIA" class="form-control"/>
                 <small class="text-danger" v-show="errors.has('nombre fantasia')">{{ errors.first('nombre fantasia') }}</small>
@@ -256,9 +260,12 @@
               <div class="form-group margin-top">
                 <h2 class="text-center">Al guardar cambios, por favor reinicie sesión para ver los efectos</h2>
               </div>
-              <button type="submit" class="btn btn-default" >Guardar</button>
+              <div class="row justify-content-center pb-3">
+                <button type="submit" class="btn btn-default" >Guardar</button>
+              </div>
             </div>
           </form>
+          </div>
         </div>
       </div>
             <!-- CONFIGURACION DE CONTRASEÑA -->
@@ -267,7 +274,8 @@
           <h2 class="text-center"><span>Configuracion de Contraseña</span></h2>
           <hr>
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-6 mx-auto">
+        <div class="row justify-content-center">
           <form @submit.prevent="validateBeforeSubmit">
             <div class="form-group margin-top">
               <label for="pass">Contraseña</label><span style="color: grey"> (*No utilice palabras o números de conocimiento público)</span>
@@ -279,8 +287,11 @@
               <input type="password" data-vv-as="contraseña" name="pass2" v-model.trim="user.pass2" class="form-control"/>
               <small class="text-danger" v-if="dataErrorMsg.error_pw">{{ dataErrorMsg.error_pw }}</small>
             </div>
+            <div class="row justify-content-center pb-3">
               <button type="submit" class="btn btn-default" >Guardar</button>
+            </div>
               </form>
+              </div>
             </div>
         </div>
               <!-- CONFIGURACION DE CLIENTE -->
@@ -289,7 +300,8 @@
           <h2 class="text-center"><span>Datos de cliente</span></h2>
           <hr>
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-6 mx-auto">
+        <div class="row justify-content-center">
           <form @submit.prevent="validateBeforeSubmit">
             <div class="form-group margin-top">
               <label for="name">Correo Electrónico</label><span style="color: grey"> (Formato ej: cliente@correo.com)</span>
@@ -332,8 +344,11 @@
             <div class="form-group margin-top">
                 <h2 class="text-center">Al guardar cambios, por favor reinicie sesión para ver los efectos</h2>
               </div>
+            <div class="row justify-content-center pb-3">
               <button type="submit" class="btn btn-default" >Guardar</button>
+            </div>
               </form>
+              </div>
             </div>
         </div>
     </div> <!-- /container -->
