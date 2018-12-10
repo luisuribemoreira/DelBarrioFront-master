3133b43c0366db4042da32bf919cc9b8e8da304e
SebastianFeliu <sebastianfeliu97@gmail.com>
2018-12-10 12:51:39 -0300

    Se implementa cards en ofertas junto con su paginacion

--- pages/index/administracion/ofertas/index.vue
+++ pages/index/administracion/ofertas/index.vue
@@ -1,28 +1,44 @@
 <template>
-  <section class="container-fluid">
+  <section class="publication-products section">
       <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <h2 class="text-center">Ofertas</h2>
-          </div>
-        </div>
-          <div class="row"  v-if="ofertas.length > 0">
-            <transition-group name="list" tag="div">
-              <div class="col-lg-6 col-md-6" v-for="oferta in ofertas" :key="oferta.IDEN_OFERTA">
-                <nuxt-link :to="{ path: '/publicaciones/'+oferta.IDEN_PUBLICACION }">
-                  <img v-if="!oferta.publicacion.imagenes || oferta.publicacion.imagenes.length === 0" v-lazy="'/img/no-image.svg'" class="img-fluid" alt="">
-                  <img v-else v-lazy="imageUrl + oferta.publicacion.imagenes[0].URL_IMAGEN" class="img-fluid" alt="">
-                </nuxt-link>
-                <h4 class="text-center">{{ oferta.publicacion.NOMB_PUBLICACION }}</h4>
+         <h2 class="text-center">Ofertas</h2>
+          <div class="row mt-5"  v-if="paginatedData[pagination].length > 0">
+              <div class="col-lg-3 col-sm-6 text-center" v-for="oferta in paginatedData[pagination]" :key="oferta.IDEN_OFERTA">
+                <div class="card">
+                <nuxt-link class="card-img-link" :to="{ path: '/publicaciones/'+oferta.IDEN_PUBLICACION }">
+                  <img v-if="!oferta.publicacion.imagenes || oferta.publicacion.imagenes.length === 0" v-lazy="'/img/no-image.svg'" class="card-img-top" alt="">
+                  <img v-else v-lazy="imageUrl + oferta.publicacion.imagenes[0].URL_IMAGEN" class="card-img-top" alt="">
+                <div class="card-body">
+                <h4 class="card-title">{{ oferta.publicacion.NOMB_PUBLICACION }}</h4>
                 <p class="text-center">{{ oferta.publicacion.DESC_PUBLICACION.substring(0,20) }}...</p>
-                <p class="text-center">Desde {{ oferta.FECH_INICIO }} Hasta {{ oferta.FECH_TERMINO }}</p>
-                <h5 class="text-center">$ {{ oferta.NUMR_PRECIO.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.") }}</h5>
+                <p class="text-center">Desde {{ oferta.FECH_INICIO }} <br> Hasta {{ oferta.FECH_TERMINO }}</p>
+                <h5 class="card-text card-price">$ {{ oferta.NUMR_PRECIO.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.") }}</h5>
+                </div>
+                </nuxt-link>
               </div>
-            </transition-group>
+            </div>
           </div>
           <div class="mr-auto my-auto" v-else>
             <h3 class="text-center"> No existen ofertas en este preciso momento</h3>
           </div>
+          <!-- PAGINACION -->
+            <nav aria-label="Page navigation">
+            <ul class="pagination justify-content-center">
+              <li class="page-item">
+                <span aria-label="Previous" v-on:click="pagination > 0 ? pagination-- : ''">
+                  <span class="page-link" :aria-hidden="true">&laquo;</span>
+                </span>
+              </li>
+              <li class="page-item" v-bind:key="page" v-for="page in pages">
+                <span class="page-link" v-bind:class="{ 'font-weight-bold' : pagination === page - 1 }" v-on:click="pagination = page - 1">{{ page }}</span>
+              </li>
+              <li class="page-item">
+                <span aria-label="Next" v-on:click="pagination < paginatedData.length - 1 ? pagination++ : ''">
+                  <span class="page-link" :aria-hidden="true">&raquo;</span>
+                </span>
+              </li>
+            </ul>
+          </nav> <!-- /PAGINACION -->
       </div>
   </section>
 </template>
@@ -30,6 +46,7 @@
 <script>
 import controller from '~/controllers/offers'
 import moment from 'moment'
+import custompaginator from '~/controllers/custompaginator'
 
 export default {
   asyncData ({ app }) {
@@ -49,15 +66,25 @@ export default {
           if (new Date(a.FECH_CREACION) < new Date(b.FECH_CREACION)) return 1
           return 0
         })
-        return {
-          ofertas: offers
-        }
+        return custompaginator.paginate(offers, 12)
+          .then(({paginatedData}) => {
+            let pages = paginatedData.length
+            return {
+              posts: offers,
+              paginatedData,
+              pages
+            }
+          })
       })
   },
   data () {
     return {
       imageUrl: process.env.imagesUrl,
-      ofertas: []
+      ofertas: [],
+      pagination: 0,
+      pages: 0,
+      paginatedData: [[]],
+      offers: []
     }
   },
   head () {
